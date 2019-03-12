0078f501ae42075ededb27f87fc029d601480395
dzakydr <dzaky.14116041@student.itera.ac.id>
2019-03-12 12:20:28 +0700

    up adminlte

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('app.app')
+
+@section('additional-css')
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="{{ asset('adminlte/plugins/font-awesome/css/font-awesome.min.css')}}">
+  <!-- IonIcons -->
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('additional-js')
+
+@endsection
